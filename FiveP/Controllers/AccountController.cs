eb0898e1c275085aaadc83c29ce620739b8636ac
cuongembaubang <cuongembaubang@gmail.com>
2020-11-03 00:21:55 +0700

    03/11/2020

---
+++ FiveP/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiveP.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult RegisterPersonalInformation()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
